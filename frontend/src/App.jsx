70487aa753ce213dc008fc7cfc74b758f97e5f7d
Dedeepyasanjana <s.gandepalli@iitg.ac.in>
2025-06-18 13:38:45 +0530

    Added responsive feature

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,28 @@
 import React from 'react'
+import {useState} from 'react'
 import Navbar from './components/Navbar/Navbar'
 import {Route,Routes} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
+import Footer from './components/Footer/Footer.jsx'
+import LoginPopup from './components/LoginPopup/LoginPopup'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
+
 const App = () => {
+  const [showLogin,setShowLogin]=useState(false)
   return (
+    <>
+   {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>} 
     <div className='app'>
-      <Navbar/>
+      <Navbar setShowLogin={setShowLogin}/>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/cart' element={<Cart/>}/>
         <Route path='/placeorder' element={<PlaceOrder/>}/>
       </Routes>
     </div>
+    <Footer/>
+    </>
   )
 }
 
--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,24 +3,16 @@ import './FoodDisplay.css';
 import { StoreContext } from '../../context/StoreContext';
 import FoodItem from '../FoodItem/FoodItem';
 
-const FoodDisplay = () => {
+const FoodDisplay = ({category}) => {
     const { food_list } = useContext(StoreContext);
-
     return (
         <div className="food-display" id='food-display'>
             <h2>Top menu for you</h2>
             <div className="food-display-list">
                 {food_list.map((item, index) => {
-                    return (
-                        <FoodItem
-                            key={index}
-                            id={item._id}
-                            name={item.name}
-                            description={item.description}
-                            price={item.price}
-                            image={item.image}
-                        />
-                    );
+                    if(category==="All"||category===item.category){
+                    return <FoodItem  key={index}  id={item._id}  name={item.name}  description={item.description}   price={item.price}    image={item.image} />
+                    }
                 })}
             </div>
         </div>
@@ -28,3 +20,4 @@ const FoodDisplay = () => {
 };
 
 export default FoodDisplay;
+ 
