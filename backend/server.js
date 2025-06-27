707a57e8e3802fec0c6710548016e2a49a1f758b
Dedeepyasanjana <s.gandepalli@iitg.ac.in>
2025-06-27 21:46:35 +0530

    Setup order and payment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import { connectDB } from './config/db.js'
 import foodRouter from './routes/foodRoute.js'
-
+import cartRouter from './routes/cartRoute.js'
+import userRouter from './routes/userRoute.js'
+import 'dotenv/config'
+import orderRouter from './routes/orderRoute.js'
 
 
 //app config 
@@ -14,13 +17,15 @@ app.use(express.json())
 app.use(cors())
 app.use('/uploads', express.static('uploads'));
 
-
 //db connection
 connectDB();
 
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images",express.static('uploads'))
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
+app.use("/api/order",orderRouter)
 
 app.get("/",(req,res)=>{
    res.send("API Working")
